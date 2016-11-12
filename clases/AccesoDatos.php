2028c51adc3f7199d6f86c8725557bd864c26da0
SalvatierraFran <franco14_93@hotmail.com>
2016-11-12 00:23:54 -0300

    Se completó la clase AccesoDatos y parte de Usuario.

--- clases/AccesoDatos.php
+++ clases/AccesoDatos.php
@@ -2,28 +2,45 @@
 class AccesoDatos
 {
 		//IMPLEMENTAR...
+    private static $ObjetoAccesoDatos;
+    private $objetoPDO;
+
     private function __construct()
     {
 		//IMPLEMENTAR...
+        try {
+            $this->objetoPDO = new PDO('mysql:host=localhost;dbname=personas;charset=utf8','root','',array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->objetoPDO->exec("SET CHARACTER SET utf8");
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage();
+            die();
+        }
     }
  
     public function RetornarConsulta($sql)
     { 
 		//IMPLEMENTAR...
+        return $this->objetoPDO->prepare($sql);
     }
     
      public function RetornarUltimoIdInsertado()
     { 
 		//IMPLEMENTAR...
+        return $this->objetoPDO->lastInsertId();
     }
  
     public static function dameUnObjetoAcceso()
     { 
 		//IMPLEMENTAR...
+        if(!isset(self::$ObjetoAccesoDatos)) {
+            self::$ObjetoAccesoDatos = new AccesoDatos();
+        }
+        return self::$ObjetoAccesoDatos;
     }
  
     public function __clone()
     { 
  		//IMPLEMENTAR...
+        trigger_error('La clonación de este objeto no está permitida', E_USER_ERROR);
     }
 }
--- clases/Usuario.php
+++ clases/Usuario.php
@@ -2,19 +2,59 @@
 
 class Usuario {
 
+    private $_id;
+    private $_nombre;
+    private $_email;
+    private $_password;
+    private $_perfil;
+    private $_foto;
+
+    //Propiedades
+    public function getId()
+    {
+        return $this->_id;
+    }
+    public function getUser()
+    {
+        return $this->_nombre;
+    }
+    public function getMail()
+    {
+         return $this->_email;
+    }
+    public function getPass()
+    {
+        return $this->_password;
+    }
+    public function getPerfil()
+    {
+        return $this->_perfil;
+    }
+    public function getFoto()
+    {
+        return $this->_foto;
+    }
+
 //--CONSTRUCTOR
     public function __construct($id = NULL) {
         if ($id !== NULL) {
 		//IMPLEMENTAR...
+            $this->_id = $id;
         }
     }
     
     public static function TraerUsuarioLogueado($obj) {
 		//IMPLEMENTAR...
+        return $this->TraerUnUsuarioPorID($obj->_id);
     }
 
     public static function TraerUnUsuarioPorId($id) {
 		//IMPLEMENTAR...
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $consulta = $objetoAccesoDato->RetornarConsulta("select * from 'usuarios' WHERE 'id'=$id");
+        $consulta->execute();
+        $arrUsuarios = $consulta->fetchAll(PDO::FECH_CLASS, "Usuario");
+        return $arrUsuarios;
     }
 
     public static function Agregar($obj) {
