f2f590b95ac4aa6fa6e6a1ff6ba5adc57f6b9558
SalvatierraFran <franco14_93@hotmail.com>
2016-11-13 20:42:24 -0300

    Se completó método Agregar

--- clases/Usuario.php
+++ clases/Usuario.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once("AccesoDatos.php");
 class Usuario {
 
     private $_id;
@@ -51,7 +51,7 @@ class Usuario {
     public static function TraerUnUsuarioPorId($id) {
 		//IMPLEMENTAR...
         $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("select * from 'usuarios' WHERE 'id'=$id");
+        $consulta = $objetoAccesoDato->RetornarConsulta("select * from usuarios WHERE id=$id");
         $consulta->execute();
         $misUsuarios = $consulta->fetchAll(PDO::FECH_CLASS, "Usuario");
         return $misUsuarios;
@@ -59,6 +59,19 @@ class Usuario {
 
     public static function Agregar($obj) {
 		//IMPLEMENTAR...
+        $conexion = AccesoDatos::dameUnObjetoAcceso();
+        $consulta = $conexion->RetornarConsulta("INSERT INTO usuarios(id, nombre, email, password, perfil, foto) VALUES (:id, :nombre, :email, :password, :perfil, :foto)");
+        $consulta->bindValue(":id", $obj->_id, PDO::PARAM_INT);
+        $consulta->bindValue(":nombre", $obj->_nombre, PDO::PARAM_STR);
+        $consulta->bindValue(":email", $obj->_email, PDO::PARAM_STR);
+        $consulta->bindValue(":password", $obj->_password, PDO::PARAM_STR);
+        $consulta->bindValue(":perfil", $obj->_perfil, PDO::PARAM_STR);
+        $consulta->bindValue(":foto", $obj->_foto, PDO::PARAM_STR);
+
+        $sentencia->Execute();
+        $id = $conexion->lastInsertId();
+        $conexion = null;
+        return $id;
     }
 
     public function ActualizarFoto() {
@@ -81,9 +94,9 @@ class Usuario {
     public static function TraerTodosLosPerfiles() {
 		//IMPLEMENTAR...
         $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
-        $consulta = $objetoAccesoDato->RetornarConsulta("select distinct 'perfil' from 'usuarios'");
+        $consulta = $objetoAccesoDato->RetornarConsulta("select distinct perfil from usuarios");
         $consulta->execute();
-        $misUsuarios = $consulta->fetchAll(PDO::FETCH_CLASS);
+        $misUsuarios = $consulta->fetchAll(PDO::FETCH_CLASS, "Usuario");
         return $misUsuarios;
     }
 
